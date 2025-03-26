e772f37e10faa7dab55e0efe82dddda058a01b09
Zenkai92 <aschierimarc@gmail.com>
2025-03-26 12:44:36 +0100

    Ajout Table settings_parties et modification des tables existantes

--- jeu_loup_ddl.sql
+++ jeu_loup_ddl.sql
@@ -1,37 +1,55 @@
 DROP TABLE IF EXISTS parties,roles,players,players_in_parties,players_play,turns;
 
+create table settings_parties(
+    id_settings int PRIMARY KEY,
+    nbr_ligne int,
+    nbr_col int,
+    wait_time int,
+    nbr_tour int,
+    nbr_obstacle int,
+    nbr_player int
+);
+
 create table parties (
-    id_party int,
+    id_party int PRIMARY KEY,
+    id_settings int,
+    FOREIGN KEY (id_settings) REFERENCES settings_parties(id_settings),
     title_party text
 );
 
 create table roles (
-    id_role int,
+    id_role int PRIMARY KEY,
     description_role text
 );
 
 create table players (
-    id_player int,
+    id_player int PRIMARY KEY,
     pseudo text
 );
 
 create table players_in_parties (
     id_party int,
+    FOREIGN KEY (id_party) REFERENCES parties(id_party),
     id_player int,
+     FOREIGN KEY (id_player) REFERENCES players(id_player),
     id_role int,
+     FOREIGN KEY (id_role) REFERENCES roles(id_role),
     is_alive text
 );
 
 create table turns (
-    id_turn int,
+    id_turn int PRIMARY KEY,
     id_party int,
+    FOREIGN KEY (id_party) REFERENCES parties(id_party),
     start_time date,
     end_time date
 );
 
 create table players_play (
     id_player int,
+     FOREIGN KEY (id_player) REFERENCES players(id_player),
     id_turn int,
+    FOREIGN KEY (id_turn) REFERENCES turns(id_turn),
     start_time date,
     end_time date,
     action varchar(10),
@@ -40,3 +58,4 @@ create table players_play (
     target_position_col text,
     target_position_row text
 );
+
