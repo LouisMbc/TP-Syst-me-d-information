a8e4fd56379f07bf4619aa5ad42c8bcbf59a039f
Zenkai92 <aschierimarc@gmail.com>
2025-03-26 12:17:15 +0100

    ajout schema DB + gitignore

---
+++ jeu_loup_ddl.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS ACHETER,CLIENT,EDITEUR,JEU_DE_SOCIETE,TYPE_JEU;
+
+create table parties (
+    id_party int,
+    title_party text
+);
+
+create table roles (
+    id_role int,
+    description_role text
+);
+
+create table players (
+    id_player int,
+    pseudo text
+);
+
+create table players_in_parties (
+    id_party int,
+    id_player int,
+    id_role int,
+    is_alive text
+);
+
+create table turns (
+    id_turn int,
+    id_party int,
+    start_time date,
+    end_time date
+);
+
+create table players_play (
+    id_player int,
+    id_turn int,
+    start_time date,
+    end_time date,
+    action varchar(10),
+    origin_position_col text,
+    origin_position_row text,
+    target_position_col text,
+    target_position_row text
+);
