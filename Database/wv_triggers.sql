162942fb842fe21f64219488beaf66d878531ae8
Zenkai92 <aschierimarc@gmail.com>
2025-03-27 12:55:21 +0100

    Mise à jour de la base de donnée et des scripts associés

--- Database/wv_triggers.sql
+++ Database/wv_triggers.sql
@@ -2,11 +2,10 @@
 CREATE TRIGGER TRG_USERNAME_TO_LOWER
 BEFORE INSERT OR UPDATE ON players
 FOR EACH ROW
-EXECUTE FUNCTION username_to_lower();
+EXECUTE FUNCTION func_username_to_lower();
 
--- Trigger : Appliquer la procédure COMPLETE_TOUR à la fin d'un tour
 CREATE TRIGGER TRG_COMPLETE_TOUR
 AFTER UPDATE ON turns
 FOR EACH ROW
 WHEN (NEW.end_time IS NOT NULL)
-EXECUTE PROCEDURE complete_tour(NEW.id_turn, NEW.id_party);
+EXECUTE FUNCTION complete_tour();
